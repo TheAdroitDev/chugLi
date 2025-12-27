2666f20ad02f0b1544fb183a4a13fb93056f1962
GitPirates <starshivamverma2@gmail.com>
2025-12-27 15:50:45 +0530

    feat: add layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <ToastContainer position="top-right" autoClose={3000} />
       </body>
     </html>
   );
